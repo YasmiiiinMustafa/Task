231318c3cd9fec44a9ca84a021b2fb5dad425ac6
YasmiiiinMustafa <yasminmostafa988@yahoo.com>
2024-02-15 10:09:56 +0200

    creating task

--- _Vois_Task/src/test/java/Task3/TestTask3.java
+++ _Vois_Task/src/test/java/Task3/TestTask3.java
@@ -16,24 +16,25 @@ import java.io.File;
 
 public class TestTask3 extends BaseModule {
 
-    @Test
-    public void TestSuccessfulSearch()
-    {
+    @Test(dataProvider = "jsonData")
+    public void TestSuccessfulSearch(String searchText) {
         task3.GetURL("https://www.google.com/ncr");
 
-        task3.SearchFor("Selenium WebDriver");
-            GoogleResult GoogleSecondResult = task3.ClickButton();
-            //Assert.assertTrue(GoogleSecondResult.GetSecondResult().contains("Selenium Webdriver Tutorial with Examples"));
+        task3.SearchFor(searchText);
+        GoogleResult GoogleSecondResult = task3.ClickButton();
+        //Assert.assertTrue(GoogleSecondResult.GetSecondResult().contains("Selenium Webdriver Tutorial with Examples"));
 
-       Assert.assertEquals(GoogleSecondResult.GetSecondResult(),"Selenium Webdriver Tutorial in Java with Examples");
+        Assert.assertEquals(GoogleSecondResult.GetSecondResult(), "Selenium Webdriver Tutorial in Java with Examples");
 
     }
 
-    @DataProvider(name= "jsonData")
-   public String  readJson()
-    {
-        String jsonFilPath = "src/test/resources/file,json";
+    @DataProvider(name = "jsonData")
+    public Object[][] readJson() throws Exception {
+        String jsonFilePath = "src/test/resources/file.json";
         JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(jsonFilePath));
 
+        String searchText = (String) jsonObject.get("text");
+        return new Object[][]{{searchText}};
     }
 }
